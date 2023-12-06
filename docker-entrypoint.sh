92f20dd83847be78906abe1253eaec77ca9f626d
daokun.lv <daokun.lv@dhc.com.cn>
2023-12-06 15:17:49 +0800

    启动前删除上次的lock

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ WIDTH=${WIDTH:-1200}
 HEIGHT=${HEIGHT:-745}
 PASSWD=${PASSWD:-123456}
 export DEBIAN_FRONTEND=noninteractive
+rm -rf /tmp/.X*-lock
 nohup Xvfb :0 -screen 0 ${WIDTH}x${HEIGHT}x24 -listen tcp -ac +extension GLX +extension RENDER >/dev/null 2>&1 &
 nohup x11vnc -forever -shared -noipv6 -passwd ${PASSWD} >/dev/null 2>&1 &
 nohup websockify --web /usr/share/novnc 8888 localhost:5900 >/dev/null 2>&1 &
